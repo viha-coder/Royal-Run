ed5be5c987b1df32f49a9229d8383973a9e12110
viha-coder <guilhermemedeiros2233@gmail.com>
2026-08-06 12:49:54 -0300

    feat: initialize Royal Run project
    - Add initial Unity project structure
    - Import Royal Run assets
    - Implement chunk spawning and movement
    - Add procedural chunk replacement system

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject chunkPrefab;
+    [SerializeField] int startingChunksAmount = 12;
+    [SerializeField] Transform chunkParent;
+    [SerializeField] float chunkLength = 10f;
+    [SerializeField] float moveSpeed = 8f;
+
+    List<GameObject> chunks = new List<GameObject>();
+
+
+    private void Start()
+    {
+        SpawnStartingChunks();
+    }
+
+    private void Update()
+    {
+        MoveChunks();
+    }
+
+    private void SpawnStartingChunks()
+    {
+        for (int i = 0; i < startingChunksAmount; i++)
+        {
+            SpawnChunk();
+        }
+    }
+
+    private void SpawnChunk()
+    {
+        float spawnPositionZ = CalculateSpawnPositionZ();
+
+        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+        GameObject newChunk = Instantiate(chunkPrefab, spawnPosition, Quaternion.identity, chunkParent);
+
+        chunks.Add(newChunk);
+    }
+
+    private float CalculateSpawnPositionZ()
+    {   
+        float spawnPositionZ;
+
+        if (chunks.Count == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            spawnPositionZ = chunks[chunks.Count - 1].transform.position.z + chunkLength;
+        }
+        return spawnPositionZ;
+    }
+
+    private void MoveChunks()
+    {
+       for (int i = 0; i < chunks.Count; i++)
+        {
+            GameObject chunk = chunks[i];
+            chunk.transform.Translate(-transform.forward * (moveSpeed * Time.deltaTime));
+
+            if (chunk.transform.position.z <= Camera.main.transform.position.z - chunkLength)
+            {
+                chunks.Remove(chunk);
+                Destroy(chunk);
+                SpawnChunk();
+            }
+        } 
+    }
+}
